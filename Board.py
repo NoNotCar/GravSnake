eb88eb2aa6cc225a29494618441a0dde1ae6c4aa
NoNotCar <13haslat@gmail.com>
2017-01-27 16:09:22 +0000

    Inefficiency I
    You gained 10 levels and got 20 lapis

--- Board.py
+++ Board.py
@@ -111,8 +111,8 @@ class Board(object):
     def grav(self):
         grav=False
         fixed=set()
-        # shapes=[t.gshape for t in self.itertiles() if t.gshape and t.grav]
-        for s in sorted(self.gshapes,key=lambda s:s.tiles[0].grav,reverse=True):
+        shapes=[t.gshape for t in self.itertiles() if t.gshape and t.grav]
+        for s in sorted(shapes,key=lambda s:s.tiles[0].grav,reverse=True):
             if s.tiles[0] in fixed:
                 continue
             cgroup=s.tiles[:]
@@ -265,15 +265,12 @@ class Board(object):
                     g.on_portal(self)
                     if g in self.updatables:
                         self.updatables.remove(g)
-                    if g.gshape in self.gshapes:
-                        self.gshapes.remove(g.gshape)
                     portal.play()
         if all([g.satisfied(self) for g in self.goals]):
             raise GameEnd(False,"WIN")
     def prepare(self):
         self.goals=[]
         self.updatables=[]
-        self.gshapes=[]
         self.game=True
         for t in self.itertiles():
             if t.interactive==2 and not self.controlled:
@@ -284,8 +281,6 @@ class Board(object):
                 self.goals.append(t)
             if t.updates:
                 self.updatables.append(t)
-            if t.gshape and t.grav and t.gshape not in self.gshapes:
-                self.gshapes.append(t.gshape)
         if self.controlled:
             self.controlled.selected=True
         self.re_img()
